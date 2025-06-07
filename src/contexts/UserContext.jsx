9b9d7c34e7deadb8898682fd0c134f30e5397887
Renan Nascimento <atomsfrontend@gmail.com>
2025-06-06 22:18:30 -0300

    fix: storage user

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,5 +1,5 @@
 // React
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 // Datas
 import users from "../data/users.json";
@@ -8,24 +8,30 @@ import users from "../data/users.json";
 import UserContext from "./CreateContext";
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
+  const [user, setUser] = useState(() => {
     const storedUser = localStorage.getItem("auth_user");
     if (storedUser) {
-      setUser(JSON.parse(storedUser));
+      return JSON.parse(storedUser);
     }
-  }, []);
+    return { email: "", name: "", role: "" };
+  });
 
   const login = (email, password) => {
+    console.log(email, password);
+
     const found = users.find(
       (user) => user.email === email && user.password === password
     );
 
     if (found) {
-      const userData = { email: found.email, name: found.name };
+      const userData = {
+        email: found.email,
+        name: found.name,
+        role: found.role,
+      };
       setUser(userData);
       localStorage.setItem("auth_user", JSON.stringify(userData));
+
       return { success: true };
     } else {
       return { success: false, message: "Credenciais inválidas." };
